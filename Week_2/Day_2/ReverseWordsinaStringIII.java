a8cd6e54a895dc0b2b181197a4c9811219dfd953
Aryan Tomar <aryantomar310707@gmail.com>
2026-06-11 21:36:49 +0530

    Create ReverseWordsinaStringIII.java

---
+++ Week_2/Day_2/ReverseWordsinaStringIII.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+       String str[] = s.split(" ");
+       StringBuilder sb = new StringBuilder();
+       for(int i=0;i<str.length;i++){
+        sb.append(new StringBuilder(str[i]).reverse());
+        if (i != str.length - 1) {
+                sb.append(" ");
+            }
+       }
+       return sb.toString();
+    }
+}
