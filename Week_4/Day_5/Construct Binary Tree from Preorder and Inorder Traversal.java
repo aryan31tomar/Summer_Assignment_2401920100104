fd4fec65296a330c563a4d275692da75d09b9b2b
Aryan Tomar <aryantomar310707@gmail.com>
2026-06-30 10:19:58 +0530

    Create Construct Binary Tree from Preorder and Inorder Traversal.java

---
+++ Week_4/Day_5/Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode helper(int[] preorder, int[] inorder,int prelo ,int prehi,int inlo , int inhi) {
+        if(prelo>prehi||inlo>inhi) return null;
+        int r=0;
+        while(inorder[r]!=preorder[prelo]) r++;
+        int leftsize=r-inlo;
+        TreeNode root = new TreeNode(preorder[prelo]);
+        root.left=helper(preorder, inorder, prelo+1, prelo+leftsize ,inlo ,r-1);
+        root.right=helper(preorder, inorder,  prelo+leftsize+1,prehi  ,r+1 ,inhi);
+        return root;
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n=preorder.length;
+        return helper(preorder, inorder , 0 ,n-1 ,0 ,n-1);
+    }
+}
